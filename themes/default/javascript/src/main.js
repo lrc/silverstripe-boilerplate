b932566c342fd036b4924ee3d1df84618302ab70
Simon Elvery <simon@elvery.net>
2012-10-24 19:21:57 +1000

    Better default main.js file.

--- themes/default/javascript/src/main.js
+++ themes/default/javascript/src/main.js
@@ -2,5 +2,16 @@
  * The site's main javascript file. Everything should stem from here with other scripts loaded dynamically using require.js
  */
 ;(function(window, undefined){
-	console.log( window );
+	
+	// Define persistent variables here.
+
+	// Kick everything off. $ = jQuery within this function.
+	require(['jquery'], function($) {
+		
+		// Wait for the DOM
+		$(function(){
+			
+		});
+	});
+
 }(window));
