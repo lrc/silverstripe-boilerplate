3d7e80b847a6a2be734e744a90c9d6c563aeaab6
Simon Elvery <simon@elvery.net>
2013-03-15 16:24:51 +1000

    Add landing page

---
+++ mysite/code/PageTypes/LandingPage.php
@@ -0,0 +1,13 @@
+<?php
+class LandingPage extends Page
+{
+	public static $description = 'A landing page suitable for use as a homepage.';
+	public static $singular_name = 'Landing Page';
+	public static $plural_name = 'Landing Pages';
+}
+
+class LandingPage_Controller extends Page_Controller
+{
+
+	
+}
